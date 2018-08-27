f9c14b37cff80b46b6d037833bc8607aa89b4cb3
Daniel Kühl Lima <dklima@br.ibm.com>
2018-08-27 20:36:25 -0300

    Arrumada indentacao

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,52 +1,47 @@
 class PetsController < ApplicationController
-	def index
-		@pets = Pet.all
-	end
+  def index
+    @pets ||= Pet.all
+  end
 
-	def show
-      @pet = Pet.find(params[:id])
+  def show
+    @pet ||= Pet.find(params[:id])
+  end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    if @pet.save
+      redirect_to @pet
+    else
+      render 'new'
     end
+  end
 
-	def new
-		@pet = Pet.new
-	end
-
-	def edit
-	  @pet = Pet.find(params[:id])
-	end
-	
-	def create
-	  @pet = Pet.new(pet_params)
- 
-      if @pet.save
-        redirect_to @pet
-      else
-      	render 'new'
-      end
-
-	end
-    
-    def update
-	  @pet = Pet.find(params[:id])
-	 
-	  if @pet.update(pet_params)
-	    redirect_to @pet
-	  else
-	    render 'edit'
-	  end
-	end
-
-	def destroy
-	  @pet = Pet.find(params[:id])
-	  @pet.destroy
-	 
-	  redirect_to pets_path
-	end
-
-    private
-    def pet_params
-      params.require(:pet).permit(:tutor, :phone, :breed, :size, :description)
+  def update
+    @pet = Pet.find(params[:id])
+    if @pet.update(pet_params)
+      redirect_to @pet
+    else
+      render 'edit'
     end
+  end
 
+  def destroy
+    @pet = Pet.find(params[:id])
+    @pet.destroy
+    redirect_to pets_path
+  end
 
+  private
+
+  def pet_params
+    params.require(:pet).permit(:tutor, :phone, :breed, :size, :description)
+  end
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,10 +1,10 @@
 class Pet < ApplicationRecord
- validates :breed, presence: true,
+  validates :breed, presence: true,
                     length: { minimum: 3 }
 
- validates :size, presence: true,
-                    length: { minimum: 3 }
+  validates :size, presence: true,
+                   length: { minimum: 3 }
 
- validates :phone, presence: true,
+  validates :phone, presence: true,
                     length: { minimum: 3 }
 end
